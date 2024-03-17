575d45c473febb4ae83a2cbb0f85354568797315
c1847017 <yiallouridess@cardiff.ac.uk>
2024-03-17 22:58:52 +0200

    Initialize project

---
+++ src/main/java/com/example/amortizationschedule/AmortizationScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.example.amortizationschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmortizationScheduleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AmortizationScheduleApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/amortizationschedule/AmortizationScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amortizationschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmortizationScheduleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
